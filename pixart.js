c2f2b93d196fc0e49500187b7286d6d74501029f
Mitche Guevara <mitche.guevara@gmail.com>
2017-07-09 17:03:15 -0400

    Enter Key Press

--- pixart.js
+++ pixart.js
@@ -1,6 +1,13 @@
-
+//change color button is clicked
 $('#set-color').on('click', function (n) {
-   ent.preventDefault()
+   n.preventDefault()
    var color = $('#color-field').val()
    $('.brush').css('background', color)
  })
+ //...when enter is pushed
+ $('#color-field').keypress(function (n) {
+    if (n.which === 13) {
+     var color = $('#color-field').val()
+      $('.brush').css('background', color)
+    }
+  })
