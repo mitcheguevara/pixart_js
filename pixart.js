4acabf76293aa7eb05abfd506d01f04e01c7350b
Mitche Guevara <mitche.guevara@gmail.com>
2017-07-09 17:49:43 -0400

    its not working

--- pixart.js
+++ pixart.js
@@ -1,23 +1,34 @@
 //change color button is clicked
+
 $('#set-color').on('click', function (n) {
    n.preventDefault()
    var color = $('#color-field').val()
    $('.brush').css('background', color)
+   colorChange()
  })
  //...when enter is pushed
  $('#color-field').keypress(function (n) {
     if (n.which === 13) {
-     var color = $('#color-field').val()
-      $('.brush').css('background', color)
+  n.preventDefault()
+  colorChange()
     }
   })
-  // create 20 squares using divs
- for (let i = 0; i < 20; i++){
-   $('body').append('<div class= "squares"></div>')
+  //coloring is fun!
+function colorChange(){
+  var color = $('color-field').val()
+
+  }
+  $('.brush').on('mouseover',function(){
+    var color = $(this).css('background')
+    brush[0].css('background',color)
+  })
+
+  // make board 8000 squares using divs
+ for (let i = 0; i < 8000; i++) {
+   $('body').append('<div class="squares"></div>')
  }
  //change color of squares to brush color
-$('.square').on('click', function(){
+$('.square').on('mouseover'), function() {
     var getColor = $('.brush').css('background')
     $(this).css('background', getColor)
-
-})
+}
